5ca69367de2fbc7e3217cebaf75dd1671442ae2a
aswini04 <aswini04081995@gmail.com>
2016-07-16 15:45:22 +0530

    finish

---
+++ README.c
@@ -0,0 +1,17 @@
+# weee
+#include<stdio.h>
+void main()
+{
+int arr[]={20,23,34,41,45,65};
+int i,j,n;
+printf("enter the number");
+scanf("%d",&n);
+for(i=0;i<=arr;i++)
+{
+for(j=1;j<=arr;j++)
+{
+if(i==j)
+}
+printf( "the repeated no %d",arr);
+}
+}
